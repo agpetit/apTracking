8d673f4d3edf5abbb51be9e3c188d6af0c153434
Frederick Roy <frederick.roy@inria.fr>
2018-03-16 08:37:04 +0100

    save diff

--- src/apCOTracking.cpp
+++ src/apCOTracking.cpp
@@ -1042,14 +1042,9 @@ grabber.acquire(Idisplay);*/
             {
             //vpImageIo::write(Ioverlaycol, filename4);
             //vpImageIo::write(Ioverlay, filename5);
-<<<<<<< HEAD
-                vpImageIo::write(Ioverlaycol, filename3);
-                vpImageIo::write(Imask, filename5);
-=======
                 //vpImageIo::write(Icol, filename4);
             //    vpImageIo::write(Imask, filename5);
 
->>>>>>> save diff
             }
 
             im++;
